c2df49ee36913243108a6845dc6d5687982be565
dalongrong <1141591465@qq.com>
2018-02-05 13:26:50 +0800

    add package

--- main.go
+++ main.go
@@ -1,36 +1,43 @@
 package main
+
 import (
 	"fmt"
-	"github.com/Masterminds/cookoo"
 
+	"github.com/Masterminds/cookoo"
+	"github.com/rongfengliang/golearning/server"
 )
-func init(){
+
+func init() {
 	fmt.Print("init main")
 }
+
 var directives = []string{
 	"example",
 	"whoami",
 }
-func main(){
-// Build a new Cookoo app.
-registry, router, context := cookoo.Cookoo()
 
-// Fill the registry.
-registry.AddRoutes(
-	cookoo.Route{
-		Name: "TEST",
-		Help: "A test route",
-		Does: cookoo.Tasks{
-			cookoo.Cmd{
-				Name: "hi",
-				Fn:   HelloWorld,
+func main() {
+	// Build a new Cookoo app.
+	p := new(server.Server)
+	p.HttpServer()
+	registry, router, context := cookoo.Cookoo()
+
+	// Fill the registry.
+	registry.AddRoutes(
+		cookoo.Route{
+			Name: "TEST",
+			Help: "A test route",
+			Does: cookoo.Tasks{
+				cookoo.Cmd{
+					Name: "hi",
+					Fn:   HelloWorld,
+				},
 			},
 		},
-	},
-)
+	)
 
-// Execute the route.
-router.HandleRequest("TEST", context, false)
+	// Execute the route.
+	router.HandleRequest("TEST", context, false)
 
 }
 func HelloWorld(cxt cookoo.Context, params *cookoo.Params) (interface{}, cookoo.Interrupt) {
